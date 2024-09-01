8165f075770d4399472ddd720fc379bdfe88e10f
iamkk25 <karthik25500@gmail.com>
2024-09-02 00:10:42 +0530

    fixed some styles in cart and modal components

--- src/components/UserDetailsForm.jsx
+++ src/components/UserDetailsForm.jsx
@@ -1,11 +1,32 @@
 import styles from './UserDetailsForm.module.scss'
 function UserDetailsForm({onClose}) {
     return <>
-        <div className={styles.userDetailForm}>
-            <div>user details form</div>
-            <button onClick={onClose}>Cancel</button>
-        </div>
-    </>
+        <form className={styles.userDetailForm}>
+            <div className={styles.inputField}>
+                <label htmlFor="fullname">Full Name:</label>
+                <input type="text" id="fullname" name="fullname" placeholder="Full Name" required/>
+            </div>
+            <div className={styles.inputField}>
+                <label htmlFor="emal">Email:</label>
+                <input type="email" id="email" name="email" placeholder="test@test.com" required/>
+            </div>
+            <div className={styles.inputField}>
+                <label htmlFor="street">Street:</label>
+                <input type="text" id="street" name="street" placeholder="enter your street" required/>
+            </div>
+            <div className={styles.inputField}>
+                <label htmlFor="city">City:</label>
+                <input type="text" id="city" name="city" placeholder="city" required/>
+            </div>
+            <div className={styles.inputField}>
+                <label htmlFor="postal-code">Postal Code:</label>
+                <input type="text" id="postal-code" name="postal-code" placeholder="600001" required/>
+            </div>
+            <div>
+                <button onClick={onClose} type={"button"}>Close</button>
+            </div>
+        </form>
+    </>;
 }
 
 export default UserDetailsForm;
